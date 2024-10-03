bfa791329bbcd4ddac2e12571e3ac3b63445909b
Neeraj <neerajgodiyal250@gmail.com>
2024-10-04 05:12:34 +0530

    Refactor print utility styles and remove download link from company name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-12">
+    <main className="container relative mx-auto scroll-my-12 overflow-auto p-4  md:p-12 ">
       {/* <Header /> */}
       <section className="mx-auto w-full max-w-2xl space-y-8   print:space-y-4">
         <div className="flex items-center justify-between">
